8b801398ff7f20c5d8ef62b8aafb394b59914373
Karl-Andreas Meus <karl.andreas@meus.ee>
2024-04-14 15:09:43 +0300

    Changed WebSecurityConfig to work with front-end

--- src/main/java/ee/cyber/manatee/config/WebSecurityConfig.java
+++ src/main/java/ee/cyber/manatee/config/WebSecurityConfig.java
@@ -17,16 +17,14 @@ public class WebSecurityConfig {
 
     @Bean
     public CorsFilter corsFilter() {
-        val configurationSource = new UrlBasedCorsConfigurationSource();
-        val corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);
-
-        // In production, use a proper allowed source list and/or reverse proxy.
-        // If you are good with CORS and decide to do Bonus Task, feel free to edit the following.
-        corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS"));
-        configurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsFilter(configurationSource);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
+
 }
